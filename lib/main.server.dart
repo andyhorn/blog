dcd8fdd8eddd92075fd07721f8840d7c94b5e422
Andy Horn <andyjhorn@gmail.com>
2026-04-22 14:47:18 -0700

    fix: code syntax highlighting

--- lib/main.server.dart
+++ lib/main.server.dart
@@ -69,7 +69,10 @@ void main() {
         script(
           src: 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js',
         ),
-        script(content: 'hljs.highlightAll();'),
+        script(
+          src: 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/dart.min.js',
+        ),
+        script(content: 'document.addEventListener("DOMContentLoaded",function(){hljs.highlightAll();});'),
         // Flutter manifest and bootstrap
         link(href: 'manifest.json', rel: 'manifest'),
         script(src: 'flutter_bootstrap.js', async: true),
